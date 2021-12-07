ebdaa35b9f4eac63ffc60cd4430ea8ea0b3f2540
pawar41 <shantanup410@gmail.com>
2021-12-07 18:33:12 +0530

    Updated var.go

--- var/var.go
+++ var/var.go
@@ -6,7 +6,11 @@ import (
 
 func main() {
 	name := "dhanesh"
-	age := 32
-
-	fmt.Println("Your name is :", name, "\nYour age is :", age)
+	age, i := 32, 0
+	var p int16 = 213
+	
+	for  i=0 ; i<=5 ; i++ {
+		fmt.Println(i+1 ,")" ,"Your name is :", name, "\nYour age is :", age)
+		fmt.Println(p)
+	}
 }
