d38ad53792e73fba49f3d83262f26b302b5c92fe
pawar41 <shantanup410@gmail.com>
2021-12-07 19:19:36 +0530

    added slice.go

---
+++ slice/slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declaring array
+	a := [5]string{"one", "two", "three", "four", "five"}
+	fmt.Println("Array after creation:", a)
+
+	var b []string = a[1:4] //created a slice named b
+	fmt.Println("Slice after creation:", b)
+
+	b[0] = "changed" // changed the slice data
+	fmt.Println("Array after slice modification:", a)
+}
